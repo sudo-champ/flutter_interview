2e16b4dfb1f9252842aaaaf3e20154c0251315db
Philips Nge <philips@getrova.com>
2024-03-08 14:54:05 +0100

    update

--- lib/util/hard_task.dart
+++ lib/util/hard_task.dart
@@ -10,11 +10,28 @@ Future<List<String>> checkForbiddenWords(TextEditingController controller) async
 
   List<String> words = text.split(' ');
 
-  
 
-  return [];
+
+  final completer = Completer<List<String>>();
+  receivePort.listen((message) {
+    if (message is List<String>) {
+      completer.complete(message);
+    }
+  });
+
+  isolate.addOnExitListener(receivePort.sendPort);
+  isolate.ping(receivePort.sendPort);
+
+  final result = await completer.future;
+  receivePort.close();
+  isolate.kill();
+
+  return result;
 }
 
 void checkText(Map<String, dynamic> message) {
+
+  final text = message['text'];
+  final sendPort = message['sendPort'];
 
 }
